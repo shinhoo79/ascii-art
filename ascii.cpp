d0791499f20a5eab5cabcef5db2f582395632a71
shinhoo <hooshin79@naver.com>
2026-03-11 21:32:28 +0900

    ascii art assignment

---
+++ ascii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "  _      _____   _   _  " << endl;
+cout << " | |    / ____| | | | | " << endl;
+cout << " | |   | (___   | |_| | " << endl;
+cout << " | |    \\___ \\  |  _  | " << endl;
+cout << " | |____ ____) | | | | |" << endl;
+cout << " |______|_____/  |_| |_|" << endl;
+
+return 0;
+}
