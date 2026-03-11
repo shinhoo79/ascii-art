56ca47b436dd857212c7d4bb415bef4279961013
shinhoo <hooshin79@naver.com>
2026-03-11 21:54:06 +0900

    add photo ascii

---
+++ photo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "           .               ,." << endl;
+cout << "          T.\"-._..---.._,-\"/|" << endl;
+cout << "          l|\"-.  _.v._   (\" |" << endl;
+cout << "          [l /.'_ \\; _~\"-.`-t" << endl;
+cout << "          Y \" _(o} _{o)._ ^.|" << endl;
+cout << "          j  T  ,-<v>-.  T  ]" << endl;
+cout << "          \\  l ( /-^-\\ ) !  !" << endl;
+cout << "           \\. \\.  \"~\"  ./  /c-..,__" << endl;
+cout << "             ^r- .._ .- .-\"  `- .  ~\"--." << endl;
+cout << "              > \\.                      \\" << endl;
+cout << "              ]   ^.                     \\" << endl;
+cout << "              3  .  \">            .       Y" << endl;
+cout << " ,.__.--._   _j   \\ ~   .         ;       |" << endl;
+cout << "(    ~\"-._~\"^._\\   ^.    ^._      I     . l" << endl;
+cout << " \"-._ ___ ~\"-,_7    .Z-._   7\"   Y      ;  \\" << endl;
+cout << "    /\"   \"~-(r r  _/_--._~-/    /      /,.--^-._" << endl;
+cout << "    \"-._    '\"~~~>-._~]>--^---./____,.^~" << endl;
+cout << "        ~--._    '   Y---." << endl;
+cout << "             ~~--._  l_   )" << endl;
+cout << "                   ~-._~~~---._,____..---" << endl;
+cout << "                       ~----\"~" << endl;
+
+return 0;
+}
